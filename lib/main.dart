aef031ef0859a923c73b5d4e7d6659cae13d58bb
Iniunam Idorenyin <iniunamid@gmail.com>
2025-04-03 12:13:39 +0000

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:waste_management_app/navigation/navigation_host_page.dart';
 // import 'package:waste_management_app/navigation/navigation_host_page.dart';
 import 'package:waste_management_app/providers/bin_provider.dart';
 import 'package:waste_management_app/resources/app_strings.dart';
 import 'package:waste_management_app/views/splash_screen.dart';
 import 'package:waste_management_app/resources/app_colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:waste_management_app/views/pages/onboarding/role_selection_page.dart';
 
 bool isBinCreated = false;
 
@@ -54,7 +52,7 @@ class MyApp extends StatelessWidget {
             surfaceTintColor: Colors.transparent,
             height: 89),
       ),
-      home: const NavigationHostPage(),
+      home: const SplashScreen(),
     );
   }
 }

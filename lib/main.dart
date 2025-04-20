725be4f6d0d3f9782d9fcf9852c1f12d1aaf4dcb
Iniunam Idorenyin <iniunamid@gmail.com>
2025-04-20 11:05:23 +0000

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:waste_management_app/providers/bin_provider.dart';
-import 'package:waste_management_app/navigation/navigation_host_page.dart';
 import 'package:waste_management_app/resources/app_strings.dart';
 import 'package:waste_management_app/views/splash_screen.dart';
 import 'package:waste_management_app/resources/app_colors.dart';
@@ -52,7 +51,7 @@ class MyApp extends StatelessWidget {
             surfaceTintColor: Colors.transparent,
             height: 89),
       ),
-      home: const NavigationHostPage(),
+      home: const SplashScreen(),
     );
   }
 }
